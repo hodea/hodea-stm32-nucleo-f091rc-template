b92e540de0ada29fb7bd1964df6a540120659cdf
Franz Hollerer <f.hollerer@gmx.net>
2017-12-10 19:16:36 +0100

    bootloader and option bytes included as binary in application

---
+++ appl/gcc/bootloader_binary.s
@@ -0,0 +1,14 @@
+# Copyright (c) 2017, Franz Hollerer.
+# SPDX-License-Identifier: MIT
+
+# Helper file to include bootloader image and the option bytes into
+# the application binary.
+# @author f.hollerer@gmx.net
+
+.section .bootloader,"a",%progbits
+
+.incbin "../../build/boot/bootloader.bin"
+
+.section .option_bytes,"a",%progbits
+
+.incbin "../../build/boot/option_bytes.bin"
